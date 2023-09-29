33d73566c4888c91916cab33d7fa07842f1cfafb
Antonio Costa <97892233+afsec@users.noreply.github.com>
2023-09-29 20:35:31 +0000

    feat: implement daisyui with vite

--- afsec/yew-debugger-v3/panel/src/main.js
+++ afsec/yew-debugger-v3/panel/src/main.js
@@ -1,5 +1,9 @@
 import './app.css';
 
-document.querySelector('#app').innerHTML = `
-  <button class="btn">Hello daisyUI</button>
-`;
+// document.querySelector('#app').innerHTML = `
+//   <button class="btn">Hello daisyUI</button>
+// `;
+
+import init from './wasm/communication_grandchild_with_grandparent.js';
+
+await init();

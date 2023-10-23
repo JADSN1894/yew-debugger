2cdcc555b623e7e8a61bb7319f2c5bf9aeaaa984
JADSN <120682244+JADSN1894@users.noreply.github.com>
2023-10-23 15:25:12 -0300

    docs: Update README.md including Getting Started subtopic

--- examples/yew-debugger-counter-layout/yew-app/src/lib.rs
+++ examples/yew-debugger-counter-layout/yew-app/src/lib.rs
@@ -10,9 +10,12 @@ use std::ops::{AddAssign, SubAssign};
 use wasm_bindgen::{prelude::wasm_bindgen, JsCast, JsValue, UnwrapThrowExt};
 use web_sys::HtmlElement;
 use yew::{html, Component, Context, Html};
+
+// * Yew Debugger Imports: 1 of 5
 use yew_debugger::impl_yew_debugger;
 use yew_debugger_derive::{YewMessage, YewModel};
 
+// * YewMessage Derive: 2 of 5
 #[derive(Debug, YewMessage)]
 pub enum Msg {
     Increment,
@@ -20,6 +23,7 @@ pub enum Msg {
     ToggleThemeMode,
 }
 
+// * YewModel Derive: 3 of 5
 #[derive(Debug, YewModel)]
 pub struct App {
     counter: CounterModel,
@@ -66,7 +70,7 @@ impl Default for App {
     }
 }
 
-// * Yew Debugger Setup: 1 of 2
+// * Yew Debugger Implementation: 4 of 5
 impl_yew_debugger!(App, Msg);
 
 impl Component for App {
@@ -78,7 +82,7 @@ impl Component for App {
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        // * Yew Debugger Setup: 2 of 2
+        // * Yew Debugger Setup: 5 of 5
         #[cfg(debug_assertions)]
         App::send_to_debbuger(self, &msg);
 
@@ -151,7 +155,7 @@ impl Component for App {
 
                                 // A button to send two Increment messages
                                 <button class="btn btn-success" onclick={ctx.link().batch_callback(|_| vec![Msg::Increment, Msg::Increment])}>
-                                    { "*2" }
+                                    { "+1 +1" }
                                 </button>
                             </div>
                         </div>
